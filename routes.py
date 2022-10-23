e9154dfbfe5a6e8a32b6beacda8bb1b6293acbc6
DragonLeo188 <59697962+DragonLeo188@users.noreply.github.com>
2022-10-23 11:50:58 +0700

    Delete routes.py

--- routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, request
-
-web = Blueprint('web', __name__)
-api = Blueprint('api', __name__)
-
-@web.route('/')
-def index():
-    return render_template('index.html')
