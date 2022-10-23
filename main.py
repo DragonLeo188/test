02944cccb28aecc2c1dc8f398a83209667f9b99c
DragonLeo188 <59697962+DragonLeo188@users.noreply.github.com>
2022-10-23 12:00:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ def index():
     return render_template('/index.html')
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4444, debug=True, use_evalex=False)
+    app.run(host='0.0.0.0', debug=True, use_evalex=False)
